50cf89096395915c9398f622522958daf7f32fc0
Quoff <ghdictio35@gmail.com>
2026-05-20 08:05:52 +0000

    Added files

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Data Communication Assignment loaded successfully.");
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.getElementById("navLinks");
+
+menuToggle.addEventListener("click", () => {
+
+  navLinks.classList.toggle("active");
+
+  if(navLinks.classList.contains("active")){
+    menuToggle.innerHTML = "✖";
+  } else {
+    menuToggle.innerHTML = "☰";
+  }
+
+});
